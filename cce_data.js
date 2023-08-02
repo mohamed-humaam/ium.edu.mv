892d2e799149f543eb02fddb9d09564a860ca223
mohamed.humaam <mohamed.humaam.athif@gmail.com>
2023-08-03 01:21:22 +0500

    as reference, updated the CCE as seperate js files

---
+++ cce_data.js
@@ -0,0 +1,171 @@
+// Sample staff data
+const CCE_Admin = [
+  {
+    name: "Aminath Raaziya",
+    occupation: "Administrative Officer",
+    photo: "./Images/Faculties/kems/admin.png",
+
+  },
+  {
+    name: "Ali Ahnaf",
+    occupation: "Administrative Officer",
+    photo: "./Images/Faculties/kems/admin1.png",
+
+  },
+  // Add more administrative staff here as needed
+];
+
+const CCE_Academic = [
+  {
+  
+    name: "Aishath Sinaau",
+    occupation: "Lecturer",
+    photo: "./Images/Faculties/kems/lecturer.png",
+
+  },
+
+  // Add more academic staff here as needed
+];
+
+
+
+
+// Sample course data (replace with your actual data)
+const CCE_Courses = [
+  {
+    name: 'Vaahaka Dhehkumuge Hunaruverikan Dhaskohdhey Course Level 1',
+    mnqfLevel: 'Level 1',
+    duration: '0.5 Years',
+    medium: 'Dhivehi',
+    fee: '900 ރ',
+    center: 'Center for Continuing Education',
+    label: 'Professional Programs'
+  },
+  {
+    name: 'Certificate 1 in Basic English',
+    mnqfLevel: 'Level 1',
+    duration: '0.5 Year',
+    medium: 'English',
+    fee: '900 ރ',
+    center: 'Center for Continuing Education',
+    label: 'Professional Programs'
+  },
+  {
+    name: 'Certificate 1 in English for General Purpose',
+    mnqfLevel: 'Level 1',
+    duration: '0.5 Year',
+    medium: 'English',
+    fee: '900 ރ',
+    center: 'Center for Continuing Education',
+    label: 'Professional Programs'
+  },
+  {
+    name: 'Certificate 2 in English for General Purpose',
+    mnqfLevel: 'Level 2',
+    duration: '0.5 Year',
+    medium: 'English',
+    fee: '2,000 ރ',
+    center: 'Center for Continuing Education',
+    label: 'Professional Programs'
+  },
+  {
+    name: 'Certificate 3 in English for General Purpose',
+    mnqfLevel: 'Level 3',
+    duration: '0.5 Year',
+    medium: 'English',
+    fee: '2,300 ރ',
+    center: 'Center for Continuing Education',
+    label: 'Professional Programs'
+  },
+  {
+    name: 'Certificate 3 for Imaams',
+    mnqfLevel: 'Level 3',
+    duration: '0.5 Year',
+    medium: 'Dhivehi',
+    fee: ',980 ރ',
+    center: 'Centre For Continuing Education',
+    label: 'Professional Programs'
+  },
+  {
+    name: 'Certificate 1 in Qira’ath al-Quran',
+    mnqfLevel: 'Level 1',
+    duration: '0.5 Year',
+    medium: 'Dhivehi',
+    fee: '300 ރ',
+    center: 'Centre For Continuing Education',
+    label: 'Professional Programs'
+  },
+  {
+    name: 'Certificate 2 in Qira’ath al-Quran',
+    mnqfLevel: 'Level 2',
+    duration: '0.5 Year',
+    medium: 'Dhievehi',
+    fee: '1,660 ރ',
+    center: 'Centre For Continuing Education',
+    label: 'Professional Programs'
+  },
+  {
+    name: 'Certificate 3 in Qira’ath al-Quran',
+    mnqfLevel: 'Level 3',
+    duration: '0.5 Year',
+    medium: 'Dhivehi',
+    fee: '2,020 ރ',
+    center: 'Centre For Continuing Education',
+    label: 'Professional Programs'
+  },
+  {
+    name: 'Advanced Certificate in English for Travel and Tourism',
+    mnqfLevel: 'Level 4',
+    duration: '1 Year',
+    medium: 'English',
+    fee: '10,240 ރ',
+    center: 'Centre For Continuing Education',
+    label: 'Professional Programs'
+  },
+  {
+    name: 'Advanced Certificate in Business Management',
+    mnqfLevel: 'Level 4',
+    duration: '1 Year',
+    medium: 'English',
+    fee: '10,620 ރ',
+    center: 'Centre For Continuing Education',
+    label: 'Professional Programs'
+  },
+  {
+    name: 'Advanced Certificate for Imaamship',
+    mnqfLevel: 'Level 4',
+    duration: '1 Year',
+    medium: 'Dhivehi',
+    fee: '9,620 ރ',
+    center: 'Centre For Continuing Education',
+    label: 'Professional Programs'
+  },
+  {
+    name: 'Advanced Certificate in Quranic Studies',
+    mnqfLevel: 'Level 4',
+    duration: '1 Year',
+    medium: 'Dhivehi',
+    fee: '9,690 ރ',
+    center: 'Centre For Continuing Education',
+    label: 'Professional Programs'
+  },
+  {
+    name: 'Advanced Certificate in Teaching',
+    mnqfLevel: 'Level 4',
+    duration: '1 Year',
+    medium: 'English',
+    fee: '9,495 ރ',
+    center: 'Centre For Continuing Education',
+    label: 'Professional Programs'
+  },
+  {
+    name: 'Advanced Certificate in Shari’ah & Law',
+    mnqfLevel: 'Level 4',
+    duration: '1 Year',
+    medium: 'Dhivehi',
+    fee: '10,665 ރ',
+    center: 'Centre For Continuing Education',
+    label: 'Professional Programs'
+  },
+  // Add more courses as needed
+];
