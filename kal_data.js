2498bbed30714f02ca6646ad906b6a26f9e9ab58
mohamed.humaam <mohamed.humaam.athif@gmail.com>
2023-08-07 02:24:19 +0500

    updated the redirect link in deans

---
+++ kal_data.js
@@ -0,0 +1,136 @@
+// Sample staff data
+const KAL_Admin = [
+  {
+    name: "Badhoora Yahya",
+    occupation: "Administrative Officer",
+    photo: "./Images/Faculties/kems/admin.png",
+
+  },
+  {
+    name: "Khadeeja Abdul Rahman",
+    occupation: "Administrative Officer",
+    photo: "./Images/Faculties/kems/admin1.png",
+
+  },
+  // Add more administrative staff here as needed
+];
+
+const KAL_Academic = [
+  // Add more academic staff here as needed
+];
+
+
+
+
+// Sample course data (replace with your actual data)
+const KAL_Courses = [
+  {
+    name: 'Certificate 1 in Arabic for Travel and Tourism',
+    mnqfLevel: 'Level 1',
+    duration: '0.5 Years',
+    medium: 'Arabic',
+    fee: '1,050 ރ',
+    center: 'Kulliyyah of Arabic Language',
+    label: 'Certificate Programs'
+  },
+  {
+    name: 'Certificate 1 in Arabic Language',
+    mnqfLevel: 'Level 1',
+    duration: '0.5 Year',
+    medium: 'Arabic',
+    fee: '300 ރ',
+    center: 'Kulliyyah of Arabic Language',
+    label: 'Certificate Programs'
+  },
+  {
+    name: 'Certificate 2 in Arabic Language',
+    mnqfLevel: 'Level 2',
+    duration: '0.5 Year',
+    medium: 'Arabic',
+    fee: '1,950 ރ',
+    center: 'Kulliyyah of Arabic Language',
+    label: 'Certificate Programs'
+  },
+  {
+    name: 'Certificate 3 in Arabic Language',
+    mnqfLevel: 'Level 3',
+    duration: '1 Year',
+    medium: 'Arabic',
+    fee: '3,600 ރ',
+    center: 'Kulliyyah of Arabic Language',
+    label: 'Certificate Programs'
+  },
+  {
+    name: 'Advanced Certificate in Arabic Language',
+    mnqfLevel: 'Level 4',
+    duration: '1 Year',
+    medium: 'Arabic',
+    fee: '11,520 ރ',
+    center: 'Kulliyyah of Arabic Language',
+    label: 'Certificate Programs'
+  },
+  {
+    name: 'Kulliyyah of Arabic Language',
+    mnqfLevel: 'Level 5',
+    duration: '1 Year',
+    medium: 'Arabic',
+    fee: '11,440 ރ',
+    center: 'Kulliyyah of Arabic Language',
+    label: 'Diploma'
+  },
+  {
+    name: 'Diploma in Arabic Language for Hospitality and Tourism',
+    mnqfLevel: 'Level 5',
+    duration: '2 Year',
+    medium: 'Arabic',
+    fee: '25,050   ރ',
+    center: 'Kulliyyah of Arabic Language',
+    label: 'Diploma'
+  },
+  {
+    name: 'Diploma in Arabic Language for Islamic Studies',
+    mnqfLevel: 'Level 5',
+    duration: '0.5 Year',
+    medium: 'Arabic',
+    fee: '13,829 ރ',
+    center: 'Kulliyyah of Arabic Language',
+    label: 'Diploma'
+  },
+  {
+    name: 'Diploma in Arabic Language for Journalism and Media',
+    mnqfLevel: 'Level 5',
+    duration: '2 Year',
+    medium: 'Arabic',
+    fee: '25,050 ރ',
+    center: 'Kulliyyah of Arabic Language',
+    label: 'Diploma'
+  },
+  {
+    name: 'Diploma in Arabic Language for Business and Marketing',
+    mnqfLevel: 'Level 5',
+    duration: '2 Year',
+    medium: 'Arabic',
+    fee: '25,050 ރ',
+    center: 'Kulliyyah of Arabic Language',
+    label: 'Diploma'
+  },
+  {
+    name: 'Diploma in Arabic Language for Diplomacy',
+    mnqfLevel: 'Level 5',
+    duration: '2 Year',
+    medium: 'Arabic',
+    fee: '25,050 ރ',
+    center: 'Kulliyyah of Arabic Language',
+    label: 'Diploma'
+  },
+  {
+    name: 'Master of Arts in Arabic Lingustics',
+    mnqfLevel: 'Level 9',
+    duration: '1 Year',
+    medium: 'Dhivehi',
+    fee: '35,370 ރ',
+    center: 'Kulliyyah of Arabic Language',
+    label: 'Masters'
+  },
+  // Add more courses as needed
+];
