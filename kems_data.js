96f30d114f532ba5ef98ab190fc0b65a1e362920
mohamed-humaam <mohamed.humaam.athif@gmail.com>
2023-08-09 13:53:29 +0500

    finidhed styling kems too. minor updates may need

---
+++ kems_data.js
@@ -0,0 +1,148 @@
+// Sample staff data
+const KEMS_Admin = [
+  {
+  
+    name: "Safwa Nashid",
+    occupation: "Administrative Officer",
+    photo: "./Images/Faculties/kems/admin.png",
+
+  },
+  {
+  
+    name: "Fathimath Nadha",
+    occupation: "Administrative Officer",
+    photo: "./Images/Faculties/kems/admin1.png",
+
+  },
+  // Add more administrative staff here as needed
+];
+
+const KEMS_Academic = [
+  {
+  
+    name: "Aishath Sinaau",
+    occupation: "Lecturer",
+    photo: "./Images/Faculties/kems/lecturer.png",
+
+  },
+  // Add more academic staff here as needed
+];
+
+
+
+//....................................................
+
+
+
+// Sample course data (replace with your actual data)
+const KEMS_Courses = [
+  {
+    name: 'Executive Master In Islamic Finance (Emif by INCEIF)',
+    mnqfLevel: 'Level 9',
+    duration: '1.5 Years',
+    medium: 'English',
+    fee: '93،522 ރ',
+    center: 'Kulliyyah of Economics & Management Studies',
+    label: 'Masters'
+  },
+  {
+    name: 'Bachelors in Local Governance and Administration',
+    mnqfLevel: 'Level 7',
+    duration: '3 Year',
+    medium: 'English',
+    fee: 'FREE',
+    center: 'Kulliyyah of Economics & Management Studies',
+    label: 'Degree'
+  },
+  {
+    name: 'Bachelor of Islamic Banking and Finance',
+    mnqfLevel: 'Level 7',
+    duration: '3 Year',
+    medium: 'English',
+    fee: 'FREE',
+    center: 'Kulliyyah of Economics & Management Studies',
+    label: 'Degree'
+  },
+  {
+    name: 'Bachelor of Human Resource Management',
+    mnqfLevel: 'Level 7',
+    duration: '3 Year',
+    medium: 'English',
+    fee: 'FREE',
+    center: 'Kulliyyah of Economics & Management Studies',
+    label: 'Degree'
+  },
+  {
+    name: 'Bachelor of Accounting and Finance',
+    mnqfLevel: 'Level 7',
+    duration: '3 Year',
+    medium: 'English',
+    fee: 'FREE',
+    center: 'Kulliyyah of Economics & Management Studies',
+    label: 'Degree'
+  },
+  {
+    name: 'Associate Degree in Local Governance and Administration',
+    mnqfLevel: 'Level 6',
+    duration: '1 Year',
+    medium: 'English',
+    fee: '20,220 ރ',
+    center: 'Kulliyyah of Economics & Management Studies',
+    label: 'Degree'
+  },
+  {
+    name: 'Associate Degree in Human Resources Management',
+    mnqfLevel: 'Level 6',
+    duration: '2 Year',
+    medium: 'English',
+    fee: '24,355 ރ',
+    center: 'Kulliyyah of Economics & Management Studies',
+    label: 'Degree'
+  },
+  {
+    name: 'Advance Diploma in Public Financial Management',
+    mnqfLevel: 'Level 6',
+    duration: '2 Year',
+    medium: 'English',
+    fee: '25,575 ރ',
+    center: 'Kulliyyah of Economics & Management Studies',
+    label: 'Degree'
+  },
+  {
+    name: 'Diploma in Local Governance and Administration',
+    mnqfLevel: 'Level 5',
+    duration: '1 Year',
+    medium: 'English',
+    fee: '10,460 ރ',
+    center: 'Kulliyyah of Economics & Management Studies',
+    label: 'Diploma'
+  },
+  {
+    name: 'Diploma in Human Resources Management',
+    mnqfLevel: 'Level 5',
+    duration: '1 Year',
+    medium: 'English',
+    fee: '13,535 ރ',
+    center: 'Kulliyyah of Economics & Management Studies',
+    label: 'Diploma'
+  },
+  {
+    name: 'Diploma in Accounting and Finance',
+    mnqfLevel: 'Level 5',
+    duration: '1 Year',
+    medium: 'English',
+    fee: '13,340 ރ',
+    center: 'Kulliyyah of Economics & Management Studies',
+    label: 'Diploma'
+  },
+  {
+    name: 'Diploma in Islamic Banking and Finance',
+    mnqfLevel: 'Level 5',
+    duration: '1 Year',
+    medium: 'English',
+    fee: '15,940 ރ',
+    center: 'Kulliyyah of Economics & Management Studies',
+    label: 'Diploma'
+  },
+  // Add more courses as needed
+];
