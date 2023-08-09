16216cdd70682e2475707ba7e8f0204d2f9a39a2
mohamed-humaam <mohamed.humaam.athif@gmail.com>
2023-08-09 12:02:07 +0500

    finished kal styling and entering data to ked

---
+++ ked_data.js
@@ -0,0 +1,208 @@
+// Sample staff data
+const KED_Admin = [
+  {
+    name: "Mariyam Nashwa",
+    occupation: "Administrative Officer",
+    photo: "./Images/Faculties/kems/admin.png",
+
+  },
+  {
+    name: "Nizna Ibrahim",
+    occupation: "Administrative Officer",
+    photo: "./Images/Faculties/kems/admin1.png",
+
+  },
+  // Add more administrative staff here as needed
+];
+
+const KED_Academic = [
+  // Add more academic staff here as needed
+];
+
+
+
+
+// Sample course data (replace with your actual data)
+const KED_Courses = [
+  {
+    name: 'Diploma in Teaching Quran',
+    mnqfLevel: 'Level 5',
+    duration: '1 Year',
+    medium: 'Dhivehi',
+    fee: '12,335 ރ',
+    center: 'Kulliyyah of Education',
+    label: 'Diploma'
+  },
+  {
+    name: 'Diploma in Teaching Islamic Studies',
+    mnqfLevel: 'Level 5',
+    duration: '1 Year',
+    medium: 'Dhivehi',
+    fee: '12,95 ރ',
+    center: 'Kulliyyah of Education',
+    label: 'Diploma'
+  },
+  {
+    name: 'Diploma in Teaching Arabic Language',
+    mnqfLevel: 'Level 5',
+    duration: '1 Year',
+    medium: 'Arabic',
+    fee: '16,080 ރ',
+    center: 'Kulliyyah of Education',
+    label: 'Diploma'
+  },
+  {
+    name: 'Diploma in Early Childhood Education',
+    mnqfLevel: 'Level 5',
+    duration: '1 Year',
+    medium: 'English',
+    fee: '11,490 ރ',
+    center: 'Kulliyyah of Education',
+    label: 'Diploma'
+  },
+  {
+    name: 'Associate Degree in Education (Primary)',
+    mnqfLevel: 'Level 6',
+    duration: '2 Years',
+    medium: 'English',
+    fee: '18,465 ރ',
+    center: 'Kulliyyah of Education',
+    label: 'Degree'
+  },
+  {
+    name: 'Associate Degree in Teaching Arabic Language',
+    mnqfLevel: 'Level 6',
+    duration: '2 Years',
+    medium: 'Arabic',
+    fee: '25,800 ރ',
+    center: 'Kulliyyah of Education',
+    label: 'Degree'
+  },
+  {
+    name: 'Associate Degree in Teaching Islamic Studies',
+    mnqfLevel: 'Level 6',
+    duration: '2 Years',
+    medium: 'Dhivehi',
+    fee: '22,705 ރ',
+    center: 'Kulliyyah of Education',
+    label: 'Degree'
+  },
+  {
+    name: 'Associate Degree in Teaching Quran',
+    mnqfLevel: 'Level 6',
+    duration: '0.5 Year',
+    medium: 'Dhivehi',
+    fee: '20,845 ރ',
+    center: 'Kulliyyah of Education',
+    label: 'Degree'
+  },
+  {
+    name: 'Bachelor of Early Childhood Education',
+    mnqfLevel: 'Level 7',
+    duration: '3 Years',
+    medium: 'English',
+    fee: '25,274 ރ',
+    center: 'Kulliyyah of Education',
+    label: 'Degree'
+  },
+  {
+    name: 'Bachelor of Education (Primary)',
+    mnqfLevel: 'Level 7',
+    duration: '3 Years',
+    medium: 'English',
+    fee: '26,820 ރ',
+    center: 'Kulliyyah of Education',
+    label: 'Degree'
+  },
+  {
+    name: 'Bachelor of Teaching Arabic Language',
+    mnqfLevel: 'Level 7',
+    duration: '3 Years',
+    medium: 'Arabic',
+    fee: '31,980 ރ',
+    center: 'Kulliyyah of Education',
+    label: 'Degree'
+  },
+  {
+    name: 'Bachelor of Teaching Islamic Studies',
+    mnqfLevel: 'Level 7',
+    duration: '3 Years',
+    medium: 'Dhivehi',
+    fee: '31,980 ރ',
+    center: 'Kulliyyah of Education',
+    label: 'Degree'
+  },
+  {
+    name: 'Bachelor of Teaching Quran',
+    mnqfLevel: 'Level 7',
+    duration: '3 Years',
+    medium: 'Dhivehi',
+    fee: '26,050 ރ',
+    center: 'Kulliyyah of Education',
+    label: 'Degree'
+  },
+  {
+    name: 'Postgraduate Diploma in Teaching',
+    mnqfLevel: 'Level 8',
+    duration: '1 Year',
+    medium: 'Dhivehi',
+    fee: '17,275 ރ',
+    center: 'Kulliyyah of Education',
+    label: 'Degree'
+  },
+  {
+    name: 'Postgraduate Certificate in TESOL (Teaching English to Speakers of Other Languages)',
+    mnqfLevel: 'Level 8',
+    duration: '0.5 Year',
+    medium: 'Dhivehi',
+    fee: '16,720 ރ',
+    center: 'Kulliyyah of Education',
+    label: 'Masters'
+  },
+  {
+    name: 'Master of Education in Leadership and Administration',
+    mnqfLevel: 'Level 9',
+    duration: '1 Year',
+    medium: 'English',
+    fee: '32,445 ރ',
+    center: 'Kulliyyah of Education',
+    label: 'Masters'
+  },
+  {
+    name: 'Master of Teaching and Learning',
+    mnqfLevel: 'Level 9',
+    duration: '1 Year',
+    medium: 'Dhivehi OR English',
+    fee: '38,280 ރ',
+    center: 'Kulliyyah of Education',
+    label: 'Masters'
+  },
+  {
+    name: 'Master of Arts in TESOL (Teaching English to Speakers of Other Languages)',
+    mnqfLevel: 'Level 9',
+    duration: '1 Year',
+    medium: 'English',
+    fee: '38,602 ރ',
+    center: 'Kulliyyah of Education',
+    label: 'Masters'
+  },
+  {
+    name: 'Master of Teaching and Learning - Islamic Education',
+    mnqfLevel: 'Level 9',
+    duration: '1 Year',
+    medium: 'Dhivehi',
+    fee: '31,035 ރ',
+    center: 'Kulliyyah of Education',
+    label: 'Masters'
+  },
+  {
+    name: 'Master of Teaching and Learning - Quran',
+    mnqfLevel: 'Level 9',
+    duration: '1 Year',
+    medium: 'Dhivehi',
+    fee: '31,035 ރ',
+    center: 'Kulliyyah of Education',
+    label: 'Masters'
+  },
+  // Add more courses as needed
+];
