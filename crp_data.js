d457cffa273d47efb9bb59975881aa466ece2137
Mohamed Humaam Athif <56443384+mohamed-humaam@users.noreply.github.com>
2023-10-08 08:04:47 +0500

    crp updated. needs touches in images and content

---
+++ crp_data.js
@@ -0,0 +1,27 @@
+// Sample staff data
+const CRP_Admin = [
+  {
+    name: "Aishath Rafaahath Ibrahim",
+    occupation: "Administrative Officer",
+    photo: "./Images/Faculties/kems/admin.png",
+
+  },
+  // Add more administrative staff here as needed
+];
+
+const CRP_Academic = [
+  {
+    name: "Dr. Visal Moosa",
+    occupation: "Senior Lecturer",
+    photo: "./Images/Faculties/kems/lecturer.png",
+  },
+  {
+    name: "Dr. Mariyam Shareefa",
+    occupation: "Senior Lecturer",
+    photo: "./Images/Faculties/kems/lecturer.png",
+  },
+  // Add more academic staff here as needed
+];
+
+
+ 
