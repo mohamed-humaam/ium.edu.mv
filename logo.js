76cde6f30c297fbaa82409f1e6ff9e52353a598f
Mohamed Humaam Athif <56443384+mohamed-humaam@users.noreply.github.com>
2023-10-09 03:16:39 +0500

    logo page made and navbar created aswell

---
+++ logo.js
@@ -0,0 +1,15 @@
+// Content for the logo
+const logoContent = `
+    <p>The logo of Islamic University of Maldives (IUM) includes several key symbols representative of and drawing inspiration from the Maldivian history, traditions, and our religion; Islam. The central element of the logo is an open book symbolizing the Qur’an. It is indicative of the fact that the basis of all knowledge and true enlightenment comes from the Qur’an. The Qur’an also encourages the pursuit of knowledge.</p>
+    <img src="your-image-url-1" alt="Qur'an Image">
+    <p>The symbol of Qur’an is encompassed in a circle representing the Minaret, a key Islamic landmark in Maldives and an important symbol of Maldivian Islamic national identity. The circle representing the Minaret is indicative of continuity of human civilization and Islamic Ummah.</p>
+    <img src="your-image-url-2" alt="Minaret Image">
+    <p>The intertwined rope represents Maldivian workmanship and is symbolic of national unity, strength, and confluence.</p>
+    <img src="your-image-url-3" alt="Rope Image">
+    <p>The Islamic star is symbolic of Islamic faith, identity, and enlightenment.</p>
+    <img src="your-image-url-4" alt="Islamic Star Image">
+`;
+
+// Add the logo content to the page
+const logoDescription = document.querySelector('.logo-description');
+logoDescription.innerHTML = logoContent;
