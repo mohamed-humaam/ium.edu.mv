03a84ec6509601535370268da5b7f758ef7bdb1a
mohamed-humaam <mohamed.humaam.athif@gmail.com>
2023-10-15 13:52:59 +0500

    style needs perfection

--- staff_strctr.js
+++ staff_strctr.js
@@ -1,173 +1,87 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const toggleButton = document.getElementById("toggleButton");
-    const staffInfo = document.getElementById("staffInfo");
-    
-    let isStaffVisible = false;
-    
-    toggleButton.addEventListener("click", function () {
-        if (isStaffVisible) {
-            // If staff information is visible, hide it and clear the content
-            staffInfo.innerHTML = "";
-            //staffInfo.style.display = "none";
-        } else {
-            // If staff information is hidden, show it
-            showKEDStaff();
-            //staffInfo.style.display = "block";
-        }
-        
-        // Toggle the flag
-        isStaffVisible = !isStaffVisible;
-    });
-    
-    function showKEDStaff() {
-        // Clear the existing content
-        staffInfo.innerHTML = "";
-        
-        // Create a list to display staff information
-        const list = document.createElement("ul");
-        
-        // Iterate through KED_Admin and add staff information to the list
-        KED_Admin.forEach(function (staff) {
-            const listItem = document.createElement("li");
-            listItem.innerHTML = `
-                <h2>${staff.name}</h2>
-                <p>Occupation: ${staff.occupation}</p>
-                <img src="${staff.photo}" alt="${staff.name}" />
-            `;
-            list.appendChild(listItem);
-        });
-        
-        // Append the list to the staffInfo div
-        staffInfo.appendChild(list);
+    const facultyData = [
+        { name: "KED", admin: KED_Admin, academic: KED_Academic },
+        { name: "KEMS", admin: KEMS_Admin, academic: KEMS_Academic },
+        { name: "KIRK", admin: KIRK_Admin, academic: KIRK_Academic },
+        { name: "KSL", admin: KSL_Admin, academic: KSL_Academic },
+        { name: "KQS", admin: KQS_Admin, academic: KQS_Academic },
+        { name: "KAL", admin: KAL_Admin, academic: KAL_Academic },
+        { name: "CCE", admin: CCE_Admin, academic: CCE_Academic },
+        { name: "CPS", admin: CPS_Admin, academic: CPS_Academic },
+        // Add more faculties here
+    ];
+
+    const facultyCardsContainer = document.getElementById("facultyCards");
+
+    function createFacultyCard(faculty) {
+        const card = document.createElement("div");
+        card.className = `faculty-card ${faculty.name.toLowerCase()}`;
+        card.textContent = faculty.name;
+
+        const adminList = createStaffList(faculty.admin, "Admin Staff");
+        const academicList = createStaffList(faculty.academic, "Academic Staff");
+
+        adminList.style.display = "none"; // Initially hidden
+        academicList.style.display = "none"; // Initially hidden
+
+        card.appendChild(adminList);
+        card.appendChild(academicList);
+
+        card.addEventListener("click", () => toggleFacultyStaff(card, adminList, academicList));
+        return card;
     }
+
+    function createStaffList(staffArray, title) {
+        const list = document.createElement("div");
+        list.className = "staff-list";
+        const titleElement = document.createElement("h4");
+        titleElement.textContent = title;
+        list.appendChild(titleElement);
+
+        staffArray.forEach(function (staff) {
+            const staffCard = createStaffCard(staff);
+            list.appendChild(staffCard);
+        });
+
+        return list;
+    }
+
+    function createStaffCard(staff) {
+        const card = document.createElement("div");
+        card.className = "staff-card";
+
+        const photo = document.createElement("img");
+        photo.src = staff.photo;
+        photo.alt = staff.name;
+        photo.className = "staff-photo";
+        card.appendChild(photo);
+
+        const name = document.createElement("p");
+        name.textContent = staff.name;
+        name.className = "staff-name";
+        card.appendChild(name);
+
+        const occupation = document.createElement("p");
+        occupation.textContent = staff.occupation;
+        occupation.className = "staff-occupation";
+        card.appendChild(occupation);
+
+        return card;
+    }
+
+    function toggleFacultyStaff(facultyCard, adminList, academicList) {
+        adminList.style.display = adminList.style.display === "none" ? "block" : "none";
+        academicList.style.display = academicList.style.display === "none" ? "block" : "none";
+        facultyCard.classList.toggle("expanded");
+    }
+
+    function initializeFacultyCards() {
+        facultyData.forEach(faculty => {
+            const card = createFacultyCard(faculty);
+            facultyCardsContainer.appendChild(card);
+        });
+    }
+
+    // Initialize faculty cards
+    initializeFacultyCards();
 });
-
-
-// Sample staff data for different faculties
-// Import data from your *_data.js files
-const faculties = [
-    {
-        name: "CCE",
-        data: {
-            admin: CCE_Admin,
-            academic: CCE_Academic
-        }
-    },
-    {
-        name: "CPS",
-        data: {
-            admin: CPS_Admin,
-            academic: CPS_Academic
-        }
-    },
-    {
-        name: "KAL",
-        data: {
-            admin: KAL_Admin,
-            academic: KAL_Academic
-        }
-    },
-    {
-        name: "KED",
-        data: {
-            admin: KED_Admin,
-            academic: KED_Academic
-        }
-    },
-    {
-        name: "KEMS",
-        data: {
-            admin: KEMS_Admin,
-            academic: KEMS_Academic
-        }
-    },
-    {
-        name: "KIRK",
-        data: {
-            admin: KIRK_Admin,
-            academic: KIRK_Academic
-        }
-    },
-    {
-        name: "KQS",
-        data: {
-            admin: KQS_Admin,
-            academic: KQS_Academic
-        }
-    },
-    {
-        name: "KSL",
-        data: {
-            admin: KSL_Admin,
-            academic: KSL_Academic
-        }
-    },
-    // Add other faculties here
-];
-
-const facultyCardsContainer = document.getElementById("facultyCards");
-const staffContainer = document.getElementById("staffContainer");
-
-function createFacultyCard(faculty) {
-    const card = document.createElement("div");
-    card.className = `faculty-card ${faculty.name.toLowerCase()}`;
-    card.textContent = faculty.name;
-    card.addEventListener("click", () => toggleFacultyStaff(faculty));
-
-    return card;
-}
-
-function createStaffCard(staff) {
-    const card = document.createElement("div");
-    card.className = "staff-card";
-
-    const photo = document.createElement("img");
-    photo.src = staff.photo;
-    photo.alt = staff.name;
-    photo.className = "staff-photo";
-    card.appendChild(photo);
-
-    const name = document.createElement("p");
-    name.textContent = staff.name;
-    name.className = "staff-name";
-    card.appendChild(name);
-
-    const occupation = document.createElement("p");
-    occupation.textContent = staff.occupation;
-    occupation.className = "staff-occupation";
-    card.appendChild(occupation);
-
-    return card;
-}
-
-function toggleFacultyStaff(faculty) {
-    console.log('Clicked:', faculty.name); // Add this line
-    
-    staffContainer.innerHTML = "";
-
-    faculty.data.admin.forEach(staff => {
-        const card = createStaffCard(staff);
-        staffContainer.appendChild(card);
-    });
-
-    faculty.data.academic.forEach(staff => {
-        const card = createStaffCard(staff);
-        staffContainer.appendChild(card);
-    });
-
-    const facultyCards = document.querySelectorAll(".faculty-card");
-    facultyCards.forEach(card => card.classList.remove("expanded"));
-    const clickedCard = document.querySelector(`.${faculty.name.toLowerCase()}`);
-    clickedCard.classList.toggle("expanded");
-}
-
-
-function initializeFacultyCards() {
-    faculties.forEach(faculty => {
-        const card = createFacultyCard(faculty);
-        facultyCardsContainer.appendChild(card);
-    });
-}
-
-// Initialize faculty cards
-initializeFacultyCards();
