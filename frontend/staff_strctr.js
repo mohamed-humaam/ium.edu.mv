384e77dd64bf43f2dd3c1d5c27aea3ca9b111ffb
mohamed-humaam <mohamed.humaam.athif@gmail.com>
2023-10-19 01:39:46 +0500

    updated js file to open staff portfolio page

--- frontend/staff_strctr.js
+++ frontend/staff_strctr.js
@@ -29,7 +29,12 @@ document.addEventListener("DOMContentLoaded", function () {
     function createFacultyCard(faculty) {
         const card = document.createElement("div");
         card.className = `faculty-card ${faculty.name.toLowerCase()}`;
-        card.textContent = faculty.name;
+
+        const facultyNameContainer = document.createElement("div");
+        facultyNameContainer.className = "faculty-name-container";
+        facultyNameContainer.textContent = faculty.name;
+
+        card.appendChild(facultyNameContainer);
 
         const adminList = createStaffList(faculty.admin, "Admin Staff");
         const academicList = createStaffList(faculty.academic, "Academic Staff");
@@ -40,33 +45,32 @@ document.addEventListener("DOMContentLoaded", function () {
         card.appendChild(adminList);
         card.appendChild(academicList);
 
-        card.addEventListener("click", () => toggleFacultyStaff(card, adminList, academicList));
+        facultyNameContainer.addEventListener("click", () => toggleFacultyStaff(card, adminList, academicList));
+
         return card;
     }
 
     function createStaffList(staffArray, title) {
         const list = document.createElement("div");
         list.className = "staff-list";
-    
-        // Create a container for the title
+
         const titleContainer = document.createElement("div");
         titleContainer.className = "title-container";
         const titleElement = document.createElement("h4");
         titleElement.textContent = title;
         titleContainer.appendChild(titleElement);
-    
-        // Create a container for the staff cards
+
         const staffContainer = document.createElement("div");
         staffContainer.className = "staff-container";
-    
+
         staffArray.forEach(function (staff) {
             const staffCard = createStaffCard(staff);
             staffContainer.appendChild(staffCard);
         });
-    
+
         list.appendChild(titleContainer);
         list.appendChild(staffContainer);
-    
+
         return list;
     }
 
@@ -90,55 +94,27 @@ document.addEventListener("DOMContentLoaded", function () {
         occupation.className = "staff-occupation";
         card.appendChild(occupation);
 
-        return card;
-    }
-
-    function createStaffCard(staff) {
-        const card = document.createElement("div");
-        card.className = "staff-card";
-    
-        const photo = document.createElement("img");
-        photo.src = staff.photo;
-        photo.alt = staff.name;
-        photo.className = "staff-photo";
-        card.appendChild(photo);
-    
-        const name = document.createElement("p");
-        name.textContent = staff.name;
-        name.className = "staff-name";
-        card.appendChild(name);
-    
-        const occupation = document.createElement("p");
-        occupation.textContent = staff.occupation;
-        occupation.className = "staff-occupation";
-        card.appendChild(occupation);
-    
-        // Create a container for contact details
         const contactContainer = document.createElement("div");
         contactContainer.className = "contact-container";
-    
-        // Create an icon for the phone and display it on the same line as the number
+
         const contactNumber = document.createElement("p");
         contactNumber.innerHTML = `<i class="fas fa-phone contact-icon"></i> ${staff.contact}`;
         contactContainer.appendChild(contactNumber);
-    
-        // Create an icon for email and display it on the same line as the email
+
         const email = document.createElement("p");
         email.innerHTML = `<i class="fas fa-envelope contact-icon"></i> ${staff.email}`;
         contactContainer.appendChild(email);
-    
+
         card.appendChild(contactContainer);
-    
+
         return card;
     }
-    
-    
+
     function toggleFacultyStaff(facultyCard, adminList, academicList) {
         adminList.style.display = adminList.style.display === "none" ? "" : "none";
         academicList.style.display = academicList.style.display === "none" ? "" : "none";
         facultyCard.classList.toggle("expanded");
     }
-    
 
     function initializeFacultyCards() {
         facultyData.forEach(faculty => {
@@ -147,6 +123,15 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
-    // Initialize faculty cards
     initializeFacultyCards();
+
+    // Add click events to administrative staff cards to redirect to staff-portfolio.html
+    facultyCardsContainer.addEventListener("click", (event) => {
+        const clickedStaffCard = event.target.closest(".staff-card");
+        if (clickedStaffCard) {
+            // Extract staff information and redirect
+            const staffName = clickedStaffCard.querySelector(".staff-name").textContent;
+            window.location.href = `staff-portfolio.html?staff=${staffName}`;
+        }
+    });
 });
