f795b552789ad98fbad8271e26de513748b7c17a
mohamed-humaam <mohamed.humaam.athif@gmail.com>
2023-10-22 21:34:06 +0500

    rules and regulation page made how

--- frontend/rules-and-regulations.js
+++ frontend/rules-and-regulations.js
@@ -1,92 +1,111 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Sample data for rules and regulations (you can replace this with your data)
-  const rulesAndRegulationsData = [
-      { title: "Academic Rule 1", category: "academic", type: "acts", pdfUrl: "academic_rule1.pdf" },
-      { title: "Administrative Rule 1", category: "administrative", type: "regulations", pdfUrl: "admin_rule1.pdf" },
-      { title: "Academic Rule 2", category: "academic", type: "acts", pdfUrl: "academic_rule2.pdf" },
-      { title: "Administrative Rule 2", category: "administrative", type: "regulations", pdfUrl: "admin_rule2.pdf" },
-      { title: "Academic Rule 3", category: "academic", type: "acts", pdfUrl: "academic_rule3.pdf" },
-      { title: "Administrative Rule 3", category: "administrative", type: "regulations", pdfUrl: "admin_rule3.pdf" },
-      { title: "Academic Rule 4", category: "academic", type: "acts", pdfUrl: "academic_rule4.pdf" },
-      { title: "Administrative Rule 4", category: "administrative", type: "regulations", pdfUrl: "admin_rule4.pdf" },
-      { title: "Academic Rule 1", category: "academic", type: "acts", pdfUrl: "academic_rule1.pdf" },
-      { title: "Administrative Rule 1", category: "administrative", type: "regulations", pdfUrl: "admin_rule1.pdf" },
-      { title: "Academic Rule 2", category: "academic", type: "acts", pdfUrl: "academic_rule2.pdf" },
-      { title: "Administrative Rule 2", category: "administrative", type: "regulations", pdfUrl: "admin_rule2.pdf" },
-      { title: "Academic Rule 3", category: "academic", type: "acts", pdfUrl: "academic_rule3.pdf" },
-      { title: "Administrative Rule 3", category: "administrative", type: "regulations", pdfUrl: "admin_rule3.pdf" },
-      { title: "Academic Rule 4", category: "academic", type: "acts", pdfUrl: "academic_rule4.pdf" },
-      { title: "Administrative Rule 4", category: "administrative", type: "regulations", pdfUrl: "admin_rule4.pdf" },
-      // Add more data objects as needed
-      // ...
-      { title: "Academic Rule 50", category: "academic", type: "acts", pdfUrl: "academic_rule50.pdf" },
-      { title: "Administrative Rule 50", category: "administrative", type: "regulations", pdfUrl: "admin_rule50.pdf" },
-  ];
+const rulesAndRegulationsData = [
+    { title: "Academic Rule 1", category: "academic", type: "acts", pdfUrl: "academic_rule1.pdf" },
+    { title: "Administrative Rule 1", category: "administrative", type: "regulations", pdfUrl: "admin_rule1.pdf" },
+    { title: "Academic Rule 2", category: "academic", type: "acts", pdfUrl: "academic_rule2.pdf" },
+    { title: "Administrative Rule 2", category: "administrative", type: "regulations", pdfUrl: "admin_rule2.pdf" },
+    { title: "Academic Rule 3", category: "academic", type: "acts", pdfUrl: "academic_rule3.pdf" },
+    { title: "Administrative Rule 3", category: "administrative", type: "regulations", pdfUrl: "admin_rule3.pdf" },
+    { title: "Academic Rule 4", category: "academic", type: "acts", pdfUrl: "academic_rule4.pdf" },
+    { title: "Administrative Rule 4", category: "administrative", type: "regulations", pdfUrl: "admin_rule4.pdf" },
+    { title: "Academic Rule 1", category: "academic", type: "acts", pdfUrl: "academic_rule1.pdf" },
+    { title: "Administrative Rule 1", category: "administrative", type: "regulations", pdfUrl: "admin_rule1.pdf" },
+    { title: "Academic Rule 2", category: "academic", type: "acts", pdfUrl: "academic_rule2.pdf" },
+    { title: "Administrative Rule 2", category: "administrative", type: "regulations", pdfUrl: "admin_rule2.pdf" },
+    { title: "Academic Rule 3", category: "academic", type: "acts", pdfUrl: "academic_rule3.pdf" },
+    { title: "Administrative Rule 3", category: "administrative", type: "regulations", pdfUrl: "admin_rule3.pdf" },
+    { title: "Academic Rule 4", category: "academic", type: "acts", pdfUrl: "academic_rule4.pdf" },
+    { title: "Administrative Rule 4", category: "administrative", type: "regulations", pdfUrl: "admin_rule4.pdf" },
+    // Add more data objects as needed
+    // ...
+    { title: "Academic Rule 50", category: "academic", type: "acts", pdfUrl: "academic_rule50.pdf" },
+    { title: "Administrative Rule 50", category: "administrative", type: "regulations", pdfUrl: "admin_rule50.pdf" },
+];
 
-  // Function to filter and display rules and regulations
-  function displayRulesAndRegulations() {
-      const filters = {
-          category: [],
-          type: [],
-      };
+const tableBody = document.getElementById("table-body");
+const categoryCheckboxes = document.querySelectorAll('.filter-checkbox');
+const typeRadios = document.querySelectorAll('input[name="doctype"]');
 
-      // Get selected category filters
-      const categoryCheckboxes = document.querySelectorAll(".filter-for-category input[type='checkbox']");
-      categoryCheckboxes.forEach((checkbox) => {
-          if (checkbox.checked) {
-              filters.category.push(checkbox.value);
-          }
-      });
+function createTableRow(data) {
+    const row = document.createElement("tr");
+    const titleCell = document.createElement("td");
+    titleCell.textContent = data.title;
+    const downloadCell = document.createElement("td");
+    const downloadIcon = document.createElement("i");
+    downloadIcon.classList.add("fa-solid", "fa-download");
+    downloadIcon.style.cursor = "pointer";
+    downloadIcon.addEventListener("click", () => {
+        downloadDocument(data.pdfUrl);
+    });
+    downloadCell.appendChild(downloadIcon);
+    const viewCell = document.createElement("td");
+    const viewIcon = document.createElement("i");
+    viewIcon.classList.add("fa-regular", "fa-eye");
+    viewIcon.style.cursor = "pointer";
+    viewIcon.addEventListener("click", () => {
+        openDocumentInNewTab(data.pdfUrl);
+    });
+    viewCell.appendChild(viewIcon);
 
-      // Get selected type filter
-      const selectedType = document.querySelector(".filter-for-type input[name='center']:checked").value;
+    row.appendChild(titleCell);
+    row.appendChild(downloadCell);
+    row.appendChild(viewCell);
+    return row;
+}
 
-      // Filter and display rules and regulations
-      const rulesAndRegulationsListContainer = document.getElementById("rules-and-regulations-list-container");
-      rulesAndRegulationsListContainer.innerHTML = "";
+function downloadDocument(pdfUrl) {
+    fetch(pdfUrl)
+        .then((response) => response.blob())
+        .then((blob) => {
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement("a");
+            a.href = url;
+            a.download = pdfUrl.split("/").pop();
+            a.style.display = "none";
+            document.body.appendChild(a);
+            a.click();
+            window.URL.revokeObjectURL(url);
+        });
+}
 
-      rulesAndRegulationsData.forEach((item) => {
-          if (
-              (filters.category.includes(item.category) || filters.category.length === 0) &&
-              (selectedType === "all" || selectedType === item.type)
-          ) {
-              const ruleItem = document.createElement("div");
-              ruleItem.className = "document-item";
+function openDocumentInNewTab(pdfUrl) {
+    window.open(pdfUrl, "_blank");
+}
 
-              // Create an Adobe Acrobat logo/icon
-              const pdfLogo = document.createElement("img");
-              pdfLogo.src = "./Images/Logo/pdf.png"; // Replace with your file path
-              pdfLogo.alt = "PDF Icon";
+function populateTable() {
+    for (const data of rulesAndRegulationsData) {
+        const row = createTableRow(data);
+        tableBody.appendChild(row);
+    }
+}
 
-              const ruleTitle = document.createElement("h3");
-              ruleTitle.textContent = item.title;
+function filterDocuments() {
+    const selectedCategories = Array.from(categoryCheckboxes)
+        .filter(checkbox => checkbox.checked)
+        .map(checkbox => checkbox.value);
 
-              // Create a link to the PDF file
-              const pdfLink = document.createElement("a");
-              pdfLink.href = item.pdfUrl;
-              pdfLink.target = "_blank"; // Open in a new tab
+    const selectedDocumentType = Array.from(typeRadios)
+        .find(radio => radio.checked)
+        .value;
 
-              // Append the logo/icon, title, and link to the document item
-              ruleItem.appendChild(pdfLogo);
-              pdfLink.appendChild(ruleTitle);
-              ruleItem.appendChild(pdfLink);
+    // Clear the table before populating it again
+    tableBody.innerHTML = '';
 
-              rulesAndRegulationsListContainer.appendChild(ruleItem);
-          }
-      });
-  }
+    for (const data of rulesAndRegulationsData) {
+        if (selectedCategories.includes(data.category) && (selectedDocumentType === 'all' || selectedDocumentType === data.type)) {
+            const row = createTableRow(data);
+            tableBody.appendChild(row);
+        }
+    }
+}
 
-  // Attach event listeners to filters
-  const categoryCheckboxes = document.querySelectorAll(".filter-for-category input[type='checkbox']");
-  categoryCheckboxes.forEach((checkbox) => {
-      checkbox.addEventListener("change", displayRulesAndRegulations);
-  });
-
-  const typeRadios = document.querySelectorAll(".filter-for-type input[name='center']");
-  typeRadios.forEach((radio) => {
-      radio.addEventListener("change", displayRulesAndRegulations);
-  });
-
-  // Initial display of rules and regulations
-  displayRulesAndRegulations();
+// Add event listeners to the filter elements
+categoryCheckboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', filterDocuments);
 });
+
+typeRadios.forEach(radio => {
+    radio.addEventListener('change', filterDocuments);
+});
+
+// Initial population
+populateTable();
