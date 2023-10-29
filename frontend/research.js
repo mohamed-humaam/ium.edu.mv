684f7a35c2f17df5100f303e69e075ef9779a474
mohamed-humaam <mohamed.humaam.athif@gmail.com>
2023-10-29 22:39:03 +0500

    document related pages updated

--- frontend/research.js
+++ frontend/research.js
@@ -8,16 +8,16 @@ const authorContainer = document.getElementById('authorContainer');
 
 const bookData = {
     'Author 1': [
-        { title: 'Book 1', genre: 'Fiction', imageUrl: 'book1.jpg' },
-        { title: 'Book 2', genre: 'Mystery', imageUrl: 'book2.jpg' },
+        { title: 'Book 1', genre: 'Fiction', imageUrl: './Images/Research/image1.jpg' },
+        { title: 'Book 2', genre: 'Mystery', imageUrl: './Images/Research/image1.jpg' },
     ],
     'Author 2': [
-        { title: 'Book 3', genre: 'Science Fiction', imageUrl: 'book3.jpg' },
-        { title: 'Book 4', genre: 'Fantasy', imageUrl: 'book4.jpg' },
+        { title: 'Book 3', genre: 'Science Fiction', imageUrl: './Images/Research/image1.jpg' },
+        { title: 'Book 4', genre: 'Fantasy', imageUrl: './Images/Research/image1.jpg' },
     ],
     'Author 3': [
-        { title: 'Book 5', genre: 'Thriller', imageUrl: 'book5.jpg' },
-        { title: 'Book 6', genre: 'Romance', imageUrl: 'book6.jpg' },
+        { title: 'Book 5', genre: 'Thriller', imageUrl: './Images/Research/image1.jpg' },
+        { title: 'Book 6', genre: 'Romance', imageUrl: './Images/Research/image1.jpg' },
     ],
 };
 
@@ -32,39 +32,46 @@ authors.forEach((author) => {
 
     const booksContainer = document.createElement('div');
     booksContainer.classList.add('book-card-container');
+    booksContainer.style.display = 'none'; // Initially hide books container
 
     authorCard.appendChild(booksContainer);
 
-    authorCard.addEventListener('click', () => {
+    authorTitle.addEventListener('click', () => {
         const books = bookData[author.name];
-        booksContainer.innerHTML = '';
+        const bookGrid = booksContainer.querySelector('.book-card-grid');
 
-        const bookGrid = document.createElement('div');
-        bookGrid.classList.add('book-card-grid');
+        if (booksContainer.style.display === 'none') {
+            booksContainer.style.display = 'block';
 
-        books.forEach((book) => {
-            const bookCard = document.createElement('div');
-            bookCard.classList.add('book-card');
-            bookCard.innerHTML = `
-                <img src="${book.imageUrl}" alt="${book.title}">
-                <div>
-                    <h3>${book.title}</h3>
-                    <p>${book.genre}</p>
-                </div>
-            `;
+            // If book grid doesn't exist, create it
+            if (!bookGrid) {
+                const bookGrid = document.createElement('div');
+                bookGrid.classList.add('book-card-grid');
 
-            // Prevent the click event on book cards from propagating to the author card
-            bookCard.addEventListener('click', (e) => {
-                e.stopPropagation();
-            });
+                books.forEach((book) => {
+                    const bookCard = document.createElement('div');
+                    bookCard.classList.add('book-card');
+                    bookCard.innerHTML = `
+                        <img src="${book.imageUrl}" alt="${book.title}">
+                        <div>
+                            <h3>${book.title}</h3>
+                            <p>${book.genre}</p>
+                        `;
 
-            bookGrid.appendChild(bookCard);
-        });
+                    bookCard.addEventListener('click', (e) => {
+                        e.stopPropagation();
+                        // Open the PDF file in a new tab when the book card is clicked
+                        window.open('./path-to-your-pdf-file.pdf', '_blank');
+                    });
 
-        booksContainer.appendChild(bookGrid);
+                    bookGrid.appendChild(bookCard);
+                });
 
-        // Toggle the "expanded" class to show or hide additional information
-        authorCard.classList.toggle('expanded');
+                booksContainer.appendChild(bookGrid);
+            }
+        } else {
+            booksContainer.style.display = 'none';
+        }
     });
 
     authorContainer.appendChild(authorCard);
