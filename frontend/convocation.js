2e65576e64f05df3dca10b0e50930f770ed4d42b
mohamed-humaam <mohamed.humaam.athif@gmail.com>
2023-11-01 12:18:29 +0500

    convocations and staff made better

--- frontend/convocation.js
+++ frontend/convocation.js
@@ -1,66 +1,95 @@
 const years = [2023, 2022, 2021];
-const yearContainer = document.getElementById('yearContainer');
+        const yearContainer = document.getElementById('yearContainer');
 
-const studentData = {
-    2023: [
-        { name: 'Student 1', batch: 'BQS36 ', imageUrl: './Images/Faculties/kems/dean.jpg' },
-        { name: 'Student 2', batch: 'BTQ17', imageUrl: 'student2.jpg' },
-    ],
-    2022: [
-        { name: 'Student 3', batch: 'BLGA12', imageUrl: 'student3.jpg' },
-        { name: 'Student 4', batch: 'BTIS33', imageUrl: 'student4.jpg' },
-    ],
-    2021: [
-        { name: 'Student 5', batch: 'MTL18', imageUrl: 'student5.jpg' },
-        { name: 'Student 6', batch: 'BQT24', imageUrl: 'student6.jpg' },
-    ],
-};
+        const studentData = {
+            2023: [
+                { name: 'Student 1', batch: 'BQS36', imageUrl: './Images/Faculties/kems/dean.jpg' },
+                { name: 'Student 2', batch: 'BTQ17', imageUrl: './Images/Faculties/kems/dean.jpg' },
+            ],
+            2022: [
+                { name: 'Student 3', batch: 'BLGA12', imageUrl: './Images/Faculties/kems/dean.jpg' },
+                { name: 'Student 4', batch: 'BTIS33', imageUrl: './Images/Faculties/kems/dean.jpg' },
+            ],
+            2021: [
+                { name: 'Student 5', batch: 'MTL18', imageUrl: './Images/Faculties/kems/dean.jpg' },
+                { name: 'Student 6', batch: 'BQT24', imageUrl: './Images/Faculties/kems/dean.jpg' },
+            ],
+        };
 
-years.forEach((year) => {
-    const yearCard = document.createElement('div');
-    yearCard.classList.add('year-card');
+        years.forEach((year) => {
+            const yearCard = document.createElement('div');
+            yearCard.classList.add('year-card');
 
-    const yearTitle = document.createElement('h2');
-    yearTitle.innerText = year;
+            const yearTitle = document.createElement('h2');
+            yearTitle.innerText = year;
 
-    yearCard.appendChild(yearTitle);
+            yearCard.appendChild(yearTitle);
 
-    const studentsContainer = document.createElement('div');
-    studentsContainer.classList.add('student-card-container');
+            const studentsContainer = document.createElement('div');
+            studentsContainer.classList.add('student-card-container');
 
-    yearCard.appendChild(studentsContainer);
+            yearCard.appendChild(studentsContainer);
 
-    yearCard.addEventListener('click', () => {
-        const students = studentData[year];
-        studentsContainer.innerHTML = '';
+            yearCard.addEventListener('click', () => {
+                const students = studentData[year];
+                studentsContainer.innerHTML = '';
 
-        const studentGrid = document.createElement('div');
-        studentGrid.classList.add('student-card-grid');
+                const studentGrid = document.createElement('div');
+                studentGrid.classList.add('student-card-grid');
 
-        students.forEach((student) => {
-            const studentCard = document.createElement('div');
-            studentCard.classList.add('student-card');
-            studentCard.innerHTML = `
-                <img src="${student.imageUrl}" alt="${student.name}">
-                <div>
-                    <h3>${student.name}</h3>
-                    <p>${student.batch}</p>
-                </div>
-            `;
+                students.forEach((student) => {
+                    const studentCard = document.createElement('div');
+                    studentCard.classList.add('student-card');
+                    studentCard.innerHTML = `
+                        <div class="student-image-container">
+                            <img src="${student.imageUrl}" alt="${student.name}" class="student-image">
+                            <div class="download-icon">
+                                <a href="${student.imageUrl}" download="${student.name}.jpg" title="Download Image">
+                                    <i class="fas fa-download"></i>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="student-name">
+                            <h3>${student.name}</h3>
+                            <p>${student.batch}</p>
+                        </div>
+                    `;
 
-            // Prevent the click event on student cards from propagating to the year card
-            studentCard.addEventListener('click', (e) => {
-                e.stopPropagation();
+                    // Prevent the click event on student cards from propagating to the year card
+                    studentCard.addEventListener('click', (e) => {
+                        e.stopPropagation();
+                    });
+
+                    const studentImage = studentCard.querySelector('.student-image');
+                    studentImage.addEventListener('click', () => {
+                        const enlargedImageContainer = document.createElement('div');
+                        enlargedImageContainer.classList.add('enlarged-image-container');
+                        enlargedImageContainer.innerHTML = `
+                            <div class="enlarged-image">
+                                <img src="${student.imageUrl}" alt="${student.name}">
+                            </div>
+                            <div class="download-icon">
+                                <a href="${student.imageUrl}" download="${student.name}.jpg" title="Download Image">
+                                    <i class="fas fa-download"></i>
+                                </a>
+                            </div>
+                        `;
+
+                        enlargedImageContainer.addEventListener('click', () => {
+                            enlargedImageContainer.remove();
+                        });
+
+                        document.body.appendChild(enlargedImageContainer);
+                    });
+
+                    studentGrid.appendChild(studentCard);
+                });
+
+                studentsContainer.appendChild(studentGrid);
+
+                // Toggle the "expanded" class to show or hide additional information
+                yearCard.classList.toggle('expanded');
             });
 
-            studentGrid.appendChild(studentCard);
+            yearContainer.appendChild(yearCard);
         });
-
-        studentsContainer.appendChild(studentGrid);
-
-        // Toggle the "expanded" class to show or hide additional information
-        yearCard.classList.toggle('expanded');
-    });
-
-    yearContainer.appendChild(yearCard);
-});
