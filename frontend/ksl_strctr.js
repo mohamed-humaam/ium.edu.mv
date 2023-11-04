8c313ba7d19a6fbacc9ff23aea1828fa039cb05f
Mohamed Humaam Athif <56443384+mohamed-humaam@users.noreply.github.com>
2023-11-04 17:46:09 +0500

    trying to link db to ksl for demo

--- frontend/ksl_strctr.js
+++ frontend/ksl_strctr.js
@@ -1,36 +1,33 @@
 // Function to create staff cards
 function createStaffCards(staffData, containerId) {
-    const staffContainer = document.getElementById(containerId);
-  
-    staffData.forEach((staff) => {
+  const staffContainer = document.getElementById(containerId);
+
+  staffData.forEach((staff) => {
       const card = document.createElement("div");
       card.classList.add("staff-card");
-  
+
       const image = document.createElement("img");
       image.src = staff.photo;
       image.alt = staff.name;
       card.appendChild(image);
-  
+
       const name = document.createElement("div");
       name.classList.add("staff-name");
       name.textContent = staff.name;
       card.appendChild(name);
-  
+
       const occupation = document.createElement("div");
       occupation.classList.add("staff-occupation");
       occupation.textContent = staff.occupation;
       card.appendChild(occupation);
-  
-      staffContainer.appendChild(card);
-    });
-  }
-  
-  // Call the function to populate the staff cards
-  createStaffCards(KSL_Admin, "kslAdmin");
-  createStaffCards(KSL_Academic, "kslAcademic");
-  
-  
 
+      staffContainer.appendChild(card);
+  });
+}
+
+// Call the function to populate the staff cards
+createStaffCards(KSL_Admin, "kslAdmin");
+createStaffCards(KSL_Academic, "kslAcademic");
 
 // Function to toggle visibility of the course table
 function toggleTableVisibility(data) {
@@ -38,12 +35,12 @@ function toggleTableVisibility(data) {
   const coursesSection = document.getElementById('courses');
 
   if (tableBody.style.display === 'none') {
-    tableBody.innerHTML = generateTableRows(data);
-    tableBody.style.display = 'table-row-group';
-    coursesSection.classList.add('show-table');
+      tableBody.innerHTML = generateTableRows(data);
+      tableBody.style.display = 'table-row-group';
+      coursesSection.classList.add('show-table');
   } else {
-    tableBody.style.display = 'none';
-    coursesSection.classList.remove('show-table');
+      tableBody.style.display = 'none';
+      coursesSection.classList.remove('show-table');
   }
 }
 
@@ -51,26 +48,26 @@ function toggleTableVisibility(data) {
 function generateTableRows(data) {
   let html = '';
   html += `
+  <tr>
+    <th>Course Name</th>
+    <th>MNQF Level</th>
+    <th>Course Duration</th>
+    <th>Medium of Instruction</th>
+    <th>Course Fee</th>
+    <th>Kulliyyah/Center</th>
+  </tr>
+`;
+  data.forEach((course) => {
+      html += `
     <tr>
-      <th>Course Name</th>
-      <th>MNQF Level</th>
-      <th>Course Duration</th>
-      <th>Medium of Instruction</th>
-      <th>Course Fee</th>
-      <th>Kulliyyah/Center</th>
+      <td>${course.name}</td>
+      <td>${course.mnqfLevel}</td>
+      <td>${course.duration}</td>
+      <td>${course.medium}</td>
+      <td>${course.fee}</td>
+      <td>${course.center}</td>
     </tr>
   `;
-  data.forEach((course) => {
-    html += `
-      <tr>
-        <td>${course.name}</td>
-        <td>${course.mnqfLevel}</td>
-        <td>${course.duration}</td>
-        <td>${course.medium}</td>
-        <td>${course.fee}</td>
-        <td>${course.center}</td>
-      </tr>
-    `;
   });
   return html;
 }
@@ -79,9 +76,9 @@ function generateTableRows(data) {
 const boxes = document.querySelectorAll('.box');
 boxes.forEach(box => {
   box.addEventListener('click', () => {
-    const boxLabel = box.textContent.trim();
-    const correspondingCourses = KSL_Courses.filter(course => course.label === boxLabel);
-    toggleTableVisibility(correspondingCourses);
+      const boxLabel = box.textContent.trim();
+      const correspondingCourses = KSL_Courses.filter(course => course.label === boxLabel);
+      toggleTableVisibility(correspondingCourses);
   });
 });
 
@@ -92,12 +89,7 @@ tableHeader.addEventListener('click', () => {
   tableBody.style.display = 'none';
 });
 
-  
-
-//....................................................
-
-
-  // Function to handle the click event on a course row
+// Function to handle the click event on a course row
 function handleCourseClick(courseName) {
   // Encode the course name to handle special characters in the URL
   const encodedCourseName = encodeURIComponent(courseName);
@@ -109,16 +101,16 @@ function handleCourseClick(courseName) {
 function generateTableRows(data) {
   let html = '';
   data.forEach((course) => {
-    html += `
-      <tr onclick="handleCourseClick('${course.name}')">
-        <td>${course.name}</td>
-        <td>${course.mnqfLevel}</td>
-        <td>${course.duration}</td>
-        <td>${course.medium}</td>
-        <td>${course.fee}</td>
-        <td>${course.center}</td>
-      </tr>
-    `;
+      html += `
+    <tr onclick="handleCourseClick('${course.name}')">
+      <td>${course.name}</td>
+      <td>${course.mnqfLevel}</td>
+      <td>${course.duration}</td>
+      <td>${course.medium}</td>
+      <td>${course.fee}</td>
+      <td>${course.center}</td>
+    </tr>
+  `;
   });
   return html;
 }
@@ -126,32 +118,30 @@ function generateTableRows(data) {
 // Call the function to generate table rows
 document.querySelector('.courses-table tbody').innerHTML = generateTableRows(KSL_Courses);
 
+// Function to fetch and display sections
+function fetchAndDisplaySections() {
+  fetch('/api/sections/')
+      .then(response => {
+          if (!response.ok) {
+              throw new Error('Network response was not ok');
+          }
+          return response.json();
+      })
+      .then(data => {
+          // You now have the Section data in the 'data' variable
+          console.log(data);
+          // You can loop through the data and display it in your frontend
+          data.forEach(section => {
+              // Display section information, e.g., in the browser's console
+              console.log(`Section Name: ${section.name}`);
+              console.log(`Message: ${section.message}`);
+              // You can update your frontend to display this information as needed
+          });
+      })
+      .catch(error => {
+          console.error('There was a problem with the fetch operation:', error);
+      });
+}
 
-
-//....................................................
-
-
-
-  // Scroll to the top of the page when the top button is clicked
-  document.querySelector('.top-button').addEventListener('click', function() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  });
-
-  // Open the map on clicking the address
-  var addressElement = document.querySelector('.info .address');
-  addressElement.addEventListener('click', function() {
-    window.open('https://www.google.com/maps?q=Islamic+University+of+Maldives');
-  });
-
-  // Dial the phone number on clicking
-  var phoneNumberElement = document.querySelector('.info .address span:nth-child(5) p');
-  phoneNumberElement.addEventListener('click', function() {
-    window.open('tel:302-100');
-  });
-
-  // Open email client on clicking the email address
-  var emailElement = document.querySelector('.info .address span:last-child p');
-  emailElement.addEventListener('click', function() {
-    window.location.href = 'mailto:info@ium.edu.mv';
-  });
-  
+// Call the function to fetch and display sections
+fetchAndDisplaySections();
