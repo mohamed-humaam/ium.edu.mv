f59d00538095d6916fb9746f162e5f74b242d969
Mohamed Humaam Athif <56443384+mohamed-humaam@users.noreply.github.com>
2023-11-05 13:22:23 +0500

    index page set

---
+++ frontend/certificate-validation.js
@@ -0,0 +1,9 @@
+// Certificate objects with their certificate numbers
+const certificates = [
+    { certificateNumber: "12345", studentName: "John Doe", studentID: "0001", courseName: "Computer Science" },
+    { certificateNumber: "54321", studentName: "Jane Smith", studentID: "0002", courseName: "Data Science" },
+    { certificateNumber: "98765", studentName: "Alice Johnson", studentID: "0003", courseName: "Mechanical Engineering" },
+    { certificateNumber: "13579", studentName: "Bob Williams", studentID: "0004", courseName: "Business Administration" },
+    { certificateNumber: "24680", studentName: "Eva Davis", studentID: "0005", courseName: "Environmental Science" },
+    { certificateNumber: "11111", studentName: "Michael Brown", studentID: "0006", courseName: "Mathematics" }
+  ];
