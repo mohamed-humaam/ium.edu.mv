f4dbe0be79bcb3d1856507961e32779c40385ae9
Mimi <69972654+Muznaa-codes@users.noreply.github.com>
2023-11-05 18:06:15 +0500

    Update hrmd.js

--- frontend/hrmd.js
+++ frontend/hrmd.js
@@ -1,16 +1,50 @@
 // script.js
 const committeeMembers = [
-    {
-        name: "John Doe",
-        universityPosition: "Professor of Mathematics",
-        committeePosition: "Chairperson",
-        photo: "./Images/Faculties/kems/dean.jpg", // Add the image file path
+   {
+        name: "Mohamed Mahid Shareef",
+        universityPosition: "Deputy Vice-Chancellor ( Administration and Finance )",
+        committeePosition: "Member",
+        photo: "./Images/tmt/DVC%20Uz%20Mahid.jpg", // Add the image file path
     },
-    {
-        name: "Jane Smith",
-        universityPosition: "Assistant Professor of History",
-        committeePosition: "Vice Chair",
-        photo: "./Images/Faculties/kems/dean.jpg", // Add the image file path
+     {
+        name: "Dr. Saudullah Ali",
+        universityPosition: "Deputy Vice-Chancellor ( Academic Affairs)",
+        committeePosition: "Member",
+        photo: "./Images/tmt/DVC%20DR%20Saud.jpg", // Add the image file path
+    },
+        {
+        name: "Dr. Dheeba Moosa",
+        universityPosition: "Deputy Vice-Chancellor (Research and Innovation)",
+        committeePosition: "Member",
+        photo: "./Images/tmt/DVC%20DR%20Dheeba.jpg", // Add the image file path
+    },
+
+     {
+        name: "Ahmed Rauf Abdulla",
+        universityPosition: "Registrar",
+        committeePosition: "Member",
+        photo: "./Images/tmt/DVC%20DR%20Dheeba.jpg", // Add the image file path
+    },
+
+     {
+        name: "Ahmadhulla Mauthoof",
+        universityPosition: "Director (Human Resource)",
+        committeePosition: "Member",
+        photo: "./Images/tmt/DVC%20DR%20Dheeba.jpg", // Add the image file path
+    },
+
+     {
+        name: "Dr. Aishath Reesha",
+        universityPosition: "Dean (Kulliyyah of Education)",
+        committeePosition: "Member",
+        photo: "./Images/tmt/DVC%20DR%20Dheeba.jpg", // Add the image file path
+    },
+
+          {
+        name: "Shafeeaa Mohamed Naseer",
+        universityPosition: "Director  (Finance)",
+        committeePosition: "Member",
+        photo: "./Images/tmt/DVC%20DR%20Dheeba.jpg", // Add the image file path
     },
     // Add more committee members here
 ];
