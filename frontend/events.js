bdca8dc6fb6ac1db131d9ebdfef679f3166fffef
Mimi <69972654+Muznaa-codes@users.noreply.github.com>
2023-11-05 17:34:29 +0500

    Update events.js

--- frontend/events.js
+++ frontend/events.js
@@ -4,7 +4,7 @@ new Vue({
         events: [
             {
                 id: 1,
-                date: '06',
+                date: '6',
                 month: 'November',
                 title: ' Chinese Ambassador Bursary program awarding ceremony',
                 description: 'We cordially extend our invitation to the China Ambassador Bursary awarding ceremony. The ceremony will take place in the IUM Hall on November 6th,2023 at 12:30 p.m.',
