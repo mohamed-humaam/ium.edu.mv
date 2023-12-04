282f431e69ebb43615c5a7d96594cf6d287744cf
mohamed-humaam-athif <mohamed.humaam.athif@gmail.com>
2023-12-04 10:51:48 +0500

    logo updated

--- frontend/logo.js
+++ frontend/logo.js
@@ -1,13 +1,22 @@
 // Content for the logo
 const logoContent = `
     <p>The logo of Islamic University of Maldives (IUM) includes several key symbols representative of and drawing inspiration from the Maldivian history, traditions, and our religion; Islam. The central element of the logo is an open book symbolizing the Qur’an. It is indicative of the fact that the basis of all knowledge and true enlightenment comes from the Qur’an. The Qur’an also encourages the pursuit of knowledge.</p>
-    <img src="your-image-url-1" alt="Qur'an Image">
-    <p>The symbol of Qur’an is encompassed in a circle representing the Minaret, a key Islamic landmark in Maldives and an important symbol of Maldivian Islamic national identity. The circle representing the Minaret is indicative of continuity of human civilization and Islamic Ummah.</p>
-    <img src="your-image-url-2" alt="Minaret Image">
-    <p>The intertwined rope represents Maldivian workmanship and is symbolic of national unity, strength, and confluence.</p>
-    <img src="your-image-url-3" alt="Rope Image">
-    <p>The Islamic star is symbolic of Islamic faith, identity, and enlightenment.</p>
-    <img src="your-image-url-4" alt="Islamic Star Image">
+    <img src="./Images/Logo/logo.svg" alt="Qur'an Image">
+
+        <div class="logo-description">
+        <div class="logo-item">
+            <img src="./Images/Logo/rope.svg" alt="Rope Image">
+            <p>The intertwined rope represents Maldivian workmanship and is symbolic of national unity, strength, and confluence.</p>
+        </div>
+        <div class="logo-item">
+            <img src="./Images/Logo/quran-symbol.svg" alt="Minaret Image">
+            <p>The symbol of Qur’an is encompassed in a circle representing the Minaret, a key Islamic landmark in Maldives and an important symbol of Maldivian Islamic national identity. The circle representing the Minaret is indicative of continuity of human civilization and Islamic Ummah.</p>
+        </div>
+        <div class="logo-item">
+            <img src="./Images/Logo/islamic-star.svg" alt="Islamic Star Image">
+            <p>The Islamic star is symbolic of Islamic faith, identity, and enlightenment.</p>
+        </div>
+        </div>
 `;
 
 // Add the logo content to the page
