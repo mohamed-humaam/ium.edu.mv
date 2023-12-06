10a8f3128cd5bf6e934fa4b97e450e13729d2d7b
mohamed-humaam-athif <mohamed.humaam.athif@gmail.com>
2023-12-06 11:57:33 +0500

    r&r, sa, scholarship done

--- frontend/sa_strctr.js
+++ frontend/sa_strctr.js
@@ -17,7 +17,7 @@ document.getElementById("dean-message").innerHTML = deanMessage;
 
 
 
-document.getElementById("intro-text").textContent = introData;
+document.getElementById("intro-text").textContent = SA_Intro;
 
 document.getElementById("vision-title").textContent = SA_Overview.vision.title;
 document.getElementById("vision-text").textContent = SA_Overview.vision.text;
@@ -66,108 +66,10 @@ function createStaffCards(staffData, containerId) {
   }
   
   // Call the function to populate the staff cards
-  createStaffCards(KQS_Admin, "kqsAdmin");
-  createStaffCards(KQS_Academic, "kqsAcademic");
-  
+  createStaffCards(SA_Admin, "saAdmin");
   
 
-
-// Function to toggle visibility of the course table
-function toggleTableVisibility(data) {
-  const tableBody = document.querySelector('.courses-table tbody');
-  const coursesSection = document.getElementById('courses');
-
-  if (tableBody.style.display === 'none') {
-    tableBody.innerHTML = generateTableRows(data);
-    tableBody.style.display = 'table-row-group';
-    coursesSection.classList.add('show-table');
-  } else {
-    tableBody.style.display = 'none';
-    coursesSection.classList.remove('show-table');
-  }
-}
-
-// Function to generate table rows for course data
-function generateTableRows(data) {
-  let html = '';
-  html += `
-    <tr>
-      <th>Course Name</th>
-      <th>MNQF Level</th>
-      <th>Course Duration</th>
-      <th>Medium of Instruction</th>
-      <th>Course Fee</th>
-      <th>Kulliyyah/Center</th>
-    </tr>
-  `;
-  data.forEach((course) => {
-    html += `
-      <tr>
-        <td>${course.name}</td>
-        <td>${course.mnqfLevel}</td>
-        <td>${course.duration}</td>
-        <td>${course.medium}</td>
-        <td>${course.fee}</td>
-        <td>${course.center}</td>
-      </tr>
-    `;
-  });
-  return html;
-}
-
-// Event listener for each box click
-const boxes = document.querySelectorAll('.box');
-boxes.forEach(box => {
-  box.addEventListener('click', () => {
-    const boxLabel = box.textContent.trim();
-    const correspondingCourses = KQS_Courses.filter(course => course.label === boxLabel);
-    toggleTableVisibility(correspondingCourses);
-  });
-});
-
-// Event listener for table header click (to hide the table when clicked again)
-const tableHeader = document.querySelector('th');
-tableHeader.addEventListener('click', () => {
-  const tableBody = document.querySelector('.courses-table tbody');
-  tableBody.style.display = 'none';
-});
-
   
-
-//....................................................
-
-
-  // Function to handle the click event on a course row
-function handleCourseClick(courseName) {
-  // Encode the course name to handle special characters in the URL
-  const encodedCourseName = encodeURIComponent(courseName);
-  const courseDetailsURL = `course_details.html?courseName=${encodedCourseName}`;
-  window.open(courseDetailsURL, '_self');
-}
-
-// Function to generate table rows for course data
-function generateTableRows(data) {
-  let html = '';
-  data.forEach((course) => {
-    html += `
-      <tr onclick="handleCourseClick('${course.name}')">
-        <td>${course.name}</td>
-        <td>${course.mnqfLevel}</td>
-        <td>${course.duration}</td>
-        <td>${course.medium}</td>
-        <td>${course.fee}</td>
-        <td>${course.center}</td>
-      </tr>
-    `;
-  });
-  return html;
-}
-
-// Call the function to generate table rows
-document.querySelector('.courses-table tbody').innerHTML = generateTableRows(KQS_Courses);
-
-
-
 //....................................................
 
 
